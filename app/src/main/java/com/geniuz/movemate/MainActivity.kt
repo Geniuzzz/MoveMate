5d719b19b27f77ec4702e9a4e542bfe239416ba8
geniuzzz <oscarvictory30@gmail.com>
2024-09-04 16:49:27 +0100

    first commit with home screen setup

---
+++ app/src/main/java/com/geniuz/movemate/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.geniuz.movemate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.geniuz.movemate.ui.screens.HomeScreen
+import com.geniuz.movemate.ui.theme.MoveMateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoveMateTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
